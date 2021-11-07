76a9a2e96ac20d86bbfdd76a73e699f3be805dc2
Diego Leonardo <diegolf125@gmail.com>
2021-11-07 20:16:43 -0300

    Improve pagination

--- frontend/src/components/pagination/index.tsx
+++ frontend/src/components/pagination/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export type Paginable = {
     first: boolean;
     last: boolean;
@@ -13,18 +15,44 @@ type Props = {
 
 const Pagination = ({ page, onPageChange }: Props) => {
 
+    const [paginationRange, setPaginationRange] = useState<number[]>([]);
+
+    // const generatePaginationRange = (): number[] => {
+    //     const paginationNumberOfOptions = (page.totalPages > 1) ? (page.totalPages / 10 < 1) ? page.totalPages : 10 : 1;        
+    //     let firstOption = page.number - Math.floor(paginationNumberOfOptions / 2);                
+    //     if (firstOption < 0) firstOption = 0;
+    //     if (firstOption + paginationNumberOfOptions > page.totalPages) firstOption = page.totalPages - paginationNumberOfOptions;
+    //     return Array.from({ length: paginationNumberOfOptions ?? 10 }, (v, k) => k + firstOption);
+    // } 
+
+    useEffect(() => {
+        const paginationNumberOfOptions = (page.totalPages > 1) ? (page.totalPages / 10 < 1) ? page.totalPages : 10 : 1;
+        let firstOption = page.number - Math.floor(paginationNumberOfOptions / 2);
+        if (firstOption < 0) firstOption = 0;
+        if (firstOption + paginationNumberOfOptions > page.totalPages) firstOption = page.totalPages - paginationNumberOfOptions;
+        setPaginationRange(Array.from({ length: paginationNumberOfOptions ?? 10 }, (v, k) => k + firstOption));
+    }, [page]);
+
     return (
-        <div className="row d-flex justify-content-center">
+        <div className="d-flex justify-content-center">
             <nav>
                 <ul className="pagination">
                     <li className={`page-item ${page.first ? 'disabled' : ''} `}>
-                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
-                    </li>                    
-                    <li className="page-item disabled">
-                        <span className="page-link">{page.number + 1}</span>
+                        <button className="page-link" onClick={() => onPageChange(0)}>Primeira</button>
                     </li>
+                    <li className={`page-item ${page.first ? 'disabled' : ''} `}>
+                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
+                    </li>
+                    {paginationRange.map(pageNumber => (
+                        <li key={pageNumber} className={`page-item ${page.number === pageNumber ? 'disabled' : ''} `}>
+                            <button className="page-link" onClick={() => onPageChange(pageNumber)}>{pageNumber}</button>
+                        </li>
+                    ))}
                     <li className={`page-item ${page.last ? 'disabled' : ''} `}>
                         <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''} `}>
+                        <button className="page-link" onClick={() => onPageChange(page.totalPages - 1)}>Última</button>
                     </li>
                 </ul>
             </nav>
